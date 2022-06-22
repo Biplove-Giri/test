55a444de5f0eb009d58ebee0cf86b5baa11337bc
Biplove Giri <garydegrim@SamsungGalaxy.local>
2022-06-22 11:02:14 +0545

    java changed and pushed to main

--- test.java
+++ test.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I am from branch naya"); 
+        System.out.println("Hello, World! I am from branch main"); 
     }
 }
 
