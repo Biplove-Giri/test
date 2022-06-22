9fcffbaa8b3402ec1eae85829f017ebbc2c8b0c1
Biplove Giri <garydegrim@SamsungGalaxy.local>
2022-06-22 17:21:05 +0545

    ami version milako hai feri

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
 
   tags = {
