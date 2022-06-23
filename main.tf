628b395a50d9c666d0805b4f5107cd42be594e56
Biplove Giri <garydegrim@SamsungGalaxy.local>
2022-06-23 10:19:44 +0545

    required version hatako

--- main.tf
+++ main.tf
@@ -2,12 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.74.0"
+      version = "~> 3.74.1"
     }
   }
 
-  required_version = ">= 1.0.0"
-}
 
 provider "aws" {
   region  = "us-east-1"
