70b6bb4a06d607d3e1e18def2f4d734833cce25f
Biplove Giri <garydegrim@SamsungGalaxy.local>
2022-07-29 15:06:45 +0545

    made some changes

--- main.tf
+++ main.tf
@@ -36,8 +36,5 @@ data "aws_secretsmanager_secret" "example" {
   arn = aws_secretsmanager_secret.example.arn
 }
 
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = data.aws_secretsmanager_secret.example.arn
-}
 
 
